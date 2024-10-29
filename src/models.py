517d69f2babece30d23f91e9ac907a6bd263b75b
Tristan Speer <speert2004@gmail.com>
2024-10-29 12:20:40 -0600

    modified __init__.py, touched up models.py

--- src/models.py
+++ src/models.py
@@ -1,11 +1,19 @@
+import uuid
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 # create a declarative base that holds table objects
 Base = declarative_base()
 
+# 
 # creating db relationships
 
 class passmanUser(Base):
-    __tablename__ = 'passman_user'
-    
+    __tablename__ = 'user'
+               # data type,                   # lambda function so each uuid (str w/ len 36) is unique
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    password_list_id = Column(String(10), ForeignKey=True)
+    username = Column(String(10), nullable=False, unique=True)
+
+class passManPassword(Base):
+    __tablename__ = 'password'
