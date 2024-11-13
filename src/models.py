637e8c640d59fe06401849aa01d449e85ca1825d
Tristan Speer <speert2004@gmail.com>
2024-11-13 14:37:33 -0700

    rough outline of models, could use some feedback

--- src/models.py
+++ src/models.py
@@ -5,15 +5,17 @@ from sqlalchemy.ext.declarative import declarative_base
 # create a declarative base that holds table objects
 Base = declarative_base()
 
-# 
 # creating db relationships
 
 class passmanUser(Base):
     __tablename__ = 'user'
                # data type,                   # lambda function so each uuid (str w/ len 36) is unique
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    password_list_id = Column(String(10), ForeignKey=True)
+    password_list_id = Column(String(10), ForeignKey('password.id'))
     username = Column(String(10), nullable=False, unique=True)
 
 class passManPassword(Base):
     __tablename__ = 'password'
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    key = Column(String(), unique=True)
+    password = Column(String(), unique = True)
